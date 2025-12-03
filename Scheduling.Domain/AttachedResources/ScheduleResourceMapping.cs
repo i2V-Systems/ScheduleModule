f0bd3c4a2633f3356b04f86381cbc9bc0aa30737
Ishita-i2v <ishita.singh@i2vsys.com>
2025-12-03 18:11:14 +0530

    refactor: improve ScheduleResourceMapping and SchedulingController with enhanced constraint handling and resource management

--- Scheduling.Domain/AttachedResources/ScheduleResourceMapping.cs
+++ Scheduling.Domain/AttachedResources/ScheduleResourceMapping.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Newtonsoft.Json;
 using Scheduling.Contracts;
 using Scheduling.Contracts.AttachedResources.Enums;
 
@@ -7,10 +8,10 @@ namespace Domain.AttachedResources;
 
 public class ScheduleResourceMapping :BaseEntity
 {
-   
+
     public Guid ScheduleId { get; set; }
     public Guid ResourceId { get; set; }
-    
+
     public Resources ResourceType { get; set; }
 
     public string? metaData { get; set; }
@@ -19,11 +20,11 @@ public class ScheduleResourceMapping :BaseEntity
     {
     }
 
-    public ScheduleResourceMapping(Guid resId, Guid schId, Resources type, dynamic data)
+    public ScheduleResourceMapping(Guid resId, Guid schId, Resources type, object? data)
     {
         ScheduleId = schId;
         ResourceId = resId;
         ResourceType = type;
-        metaData = data;
+        metaData = data != null ? JsonConvert.SerializeObject(data) : null;
     }
 }
--- Scheduling.Infrastructure/ScheduleScripts/scheduleScripts.sql
+++ Scheduling.Infrastructure/ScheduleScripts/scheduleScripts.sql
@@ -62,8 +62,18 @@ ALTER TABLE public."ScheduleResourceMapping"
 DROP CONSTRAINT if exists uk_schedule_resource_type;
 
 DO $$
+BEGIN
+    IF  EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conname = 'uk_schedule_resource'
+    ) THEN
+ALTER TABLE public."ScheduleResourceMapping"
+    DROP CONSTRAINT uk_schedule_resource;
+END IF;
+END$$;
 
-
+DO $$
 BEGIN
     IF NOT EXISTS (
         SELECT 1
@@ -71,11 +81,10 @@ BEGIN
         WHERE conname = 'uk_schedule_resource'
     ) THEN
 ALTER TABLE public."ScheduleResourceMapping"
-    ADD CONSTRAINT uk_schedule_resource
-        UNIQUE ("Id");
+  ADD CONSTRAINT uk_schedule_resource
+    UNIQUE ("ScheduleId", "ResourceId", "ResourceType");
 END IF;
 END$$;
-
 
 DO $$
 BEGIN
