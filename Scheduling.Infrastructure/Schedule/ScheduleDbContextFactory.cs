2a2d63f453053298a1334c5e906aa600348c6049
PatwalVineet <vineet.patwal@i2vsys.com>
2026-04-22 16:28:15 +0530

    refactor: remove ScheduleDbContextFactory class

--- Scheduling.Infrastructure/Schedule/ScheduleDbContextFactory.cs
+++
@@ -1,9 +0,0 @@
-using Common.Infrastructure.Data.Factories;
-
-namespace Infrastructure.Schedule;
-
-public class ScheduleDbContextFactory : DbContextFactoryBase<ScheduleDbContext>
-{
-    protected override string MigrationsAssembly => "Scheduling.Infrastructure";
-    protected override string MigrationHistorySchema => Schemas.Schedule;
-}
