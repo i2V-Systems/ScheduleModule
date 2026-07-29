44d9ddf838bac59a5934f060210c312fa0cc7b3d
Ishita-i2v <ishita.singh@i2vsys.com>
2026-07-29 18:22:50 +0530

    Remove auditing references from InfrastructureDependencyInjection and project files

--- Scheduling.Infrastructure/Extensions/InfrastructureDependencyInjection.cs
+++ Scheduling.Infrastructure/Extensions/InfrastructureDependencyInjection.cs
@@ -1,6 +1,5 @@
 
 using Application.Schedule;
-using Common.Auditing.EntityFramework;
 using Infrastructure.Schedule;
 using Microsoft.EntityFrameworkCore;
 using AutoMapper;
@@ -34,7 +33,6 @@ public static class InfrastructureDependencyInjection
                         b.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
                     })
                 .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
-                .UseAuditing()
                 .EnableSensitiveDataLogging();
         }, ServiceLifetime.Scoped);
 
